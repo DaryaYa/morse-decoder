7d109b5e49389fb618108ba52b2c1312545cceee
DaryaYa <lyalyaka@mail.ru>
2019-10-03 13:09:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -41,10 +41,18 @@ function decode(expr) {
     let letters = expr.match(/.{1,10}/g);
     console.log(letters);
 
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < letters.length; i++) {
         let words = letters[i].match(/.{1,2}/g);
-        console.log(words);
 
+        function ff(e) {
+            return (e != "00");
+        }
+        let words1 = words.filter(ff);
+        console.log(words1);
+        let words2 = words1.join("");
+        let words3 = words2.replace(/10/g, ".");
+        let words4 = words3.replace(/11/g, "-");
+        console.log(words4);
     }
 
 
@@ -52,7 +60,8 @@ function decode(expr) {
 const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 decode(expr);
 
-
+//var representation = str.replace(/([.-]+[-.]*)/g, (_, x) => dictionary[x]);
+//console.log(representation);
 //module.exports = {
 // decode
 //}
